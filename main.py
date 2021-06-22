7b5f5f9d61fbf5d055c0cb88f9a6d226ed8697f5
Niyaz-Mohamed <niyazshahul2006@gmail.com>
2021-06-22 20:05:47 +0800

    Added library and editor routes

--- main.py
+++ main.py
@@ -7,15 +7,27 @@ app=Flask(__name__)
 #Run env\Scripts\activate to activate venv
 #TASK: Add error handlers
 
-@app.route('/')
-def root():
-    with open('./static/assets/title_desc.txt', 'r') as file:
-        title_desc = file.read().replace('\n', '')
-    return render_template('index.html',title_desc=title_desc)
-
 @app.errorhandler(HTTPException)
 def handle_exception(e):
     error = {'code':e.code,'name':e.name,'description':e.description}
     return render_template('error.html', error=error)
+
+@app.route('/')
+def root():
+    with open('./static/assets/title_desc.txt', 'r') as file:
+        title_desc = file.read()
+    return render_template('index.html',title_desc=title_desc)
+
+@app.route('/base')
+def loadBase():
+    return render_template('base.html')
+
+@app.route('/editor')
+def loadBase():
+    return render_template('base.html')
+
+@app.route('/library')
+def imageLib():
+    return render_template('base.html')
 
 app.run(host='0.0.0.0', port=8080, debug=True)
