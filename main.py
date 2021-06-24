7581065c90744033dca54a6ed4d21d3aceacf338
MshNiyaz <niyazshahul2006@gmail.com>
2021-06-24 11:58:51 +0800

    Added basic file uploading to local upload folder

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 from flask import Flask
-from flask import render_template, request, json, session
+from flask import render_template, request, flash, request, redirect, url_for
 from werkzeug.exceptions import HTTPException
+from werkzeug.utils import secure_filename
 
 import os, json
+import urllib.request
+
+UPLOAD_FOLDER='./uploads'
+ALLOWED_EXTENSIONS={'png','jpg','jpeg'}
 
 app=Flask(__name__)
+app.config['UPLOAD_FOLDER']=UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def allowedFile(filename):
+    allowedExtensions = set(['png','jpg','jpeg'])
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in allowedExtensions
 
 @app.errorhandler(HTTPException)
 def handle_exception(e):
     error = {'code':e.code,'name':e.name,'description':e.description}
     return render_template('error.html', error=error)
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/')
 def root():
 
     title_desc="""Mosiac allows you to take a high-definition image and convert it to pixel art.\n 
@@ -24,9 +38,36 @@ def root():
 def loadBase():
     return render_template('base.html',noLoadFooter=True)
 
-@app.route('/editor')
-def openEditor():
-    return render_template('editor.html')
+@app.route('/editor',methods=['POST','GET'])
+def runEditor():
+
+    filename=''
+    errorMsg='No file chosen'
+
+    if request.method == 'POST':
+        
+        print('request called')
+        if 'file' not in request.files:
+            errorMsg='Error: Form did not return Image'
+            flash('No file part')
+            return redirect(request.url)
+            
+        file=request.files['file']
+
+        if file.filename =='':
+            errorMsg='Error: No file chosen'
+            flash('No selected file')
+            return redirect(request.url)
+            
+        if file and allowed_file(file.filename):
+            filename=secure_filename(file.filename)
+            print(filename)
+            try:
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+            except:
+                errorMsg('Error: Image Upload Failed')
+
+    return render_template('editor.html',filename=filename,errorMsg=errorMsg)
 
 @app.route('/library')
 def imageLib():
